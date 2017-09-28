2d683ef8a6a797b13566c611caeea70572bb454b
Di Huynh <Di.huynh@blackbaud.com>
2017-09-28 15:14:38 -0500

    initialize postgres container

--- src/main/java/racememos/RaceMemos.java
+++ src/main/java/racememos/RaceMemos.java
@@ -4,12 +4,14 @@ import com.blackbaud.boot.config.CommonSpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan({"racememos", "com.blackbaud.boot.converters"})
 @ComponentScan("racememos")
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class RaceMemos extends CommonSpringConfig {
